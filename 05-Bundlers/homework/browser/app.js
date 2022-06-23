fcbb3efd820148f80b35b04840590e33bad68508
Miguel Gallardo <miguel.gallardof@icloud.com>
2022-06-23 02:25:51 -0500

    homework Bundlers

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,33 @@
-(function () {
+//var whiteboard = window.whiteboard;
+//var socket = window.io(window.location.origin);
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
+//Common JS
+// const whiteboard = require("./whiteboard.js");
+// const io = require("socket.io-client");
+// const socket = io(window.location.origin);
 
-  socket.on('connect', function () {
-    console.log('Connected!');
-  });
+//ES6
+import whiteboard from './whiteboard.js';
+import io from 'socket.io-client';
+const socket = io(window.location.origin)
 
-  socket.on('load', function (strokes) {
+socket.on("connect", function () {
+  console.log("Connected!");
+});
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
-
-  });
-
-  socket.on('draw', function (start, end, color) {
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
+});
 
-  whiteboard.on('draw', function (start, end, color) {
-    socket.emit('draw', start, end, color);
-  });
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
 
-})();
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
